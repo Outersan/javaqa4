00e945f3fadb68fcf8ab6e0c82cf20952df4f203
Kot E <ekattow9@gmail.com>
2022-08-02 17:41:03 +0300

    first

---
+++ run.cmd
@@ -0,0 +1,1 @@
+javadoc -docletpath target\javaqa4-1.0-SNAPSHOT-jar-with-dependencies.jar -doclet org.example.Doclet %*
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+javadoc -docletpath target/javaqa4-1.0-SNAPSHOT-jar-with-dependencies.jar -doclet org.example.Doclet "$@"
